b5804863586c5412a1918ff4a71c14f6bc8f0cc7
Daniel Zarate <danielezn@hotmail.com>
2016-11-02 09:22:16 -0300

    pushing last update

--- app/models/doubt.rb
+++ app/models/doubt.rb
@@ -2,8 +2,11 @@ class Doubt < ApplicationRecord
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
   belongs_to :user
   has_many :offers, :class_name => "offer"
+  has_one :selecteld_offer, :class_name => "offer"
+
   enum privacy: [ :public_status, :private_status ]
 
   def can_offer user_id
